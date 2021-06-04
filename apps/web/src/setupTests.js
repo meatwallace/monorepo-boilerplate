661435c834d0ebb88c8f9cfd463ca155f474205c
Geoff Whatley <me@geoffwhatley.com>
2021-06-04 14:16:14 +1000

    test(#8): disable react-query logging in tests
    closes #8

--- apps/web/src/setupTests.js
+++ apps/web/src/setupTests.js
@@ -5,7 +5,15 @@ import 'jest-extended';
 // source: https://github.com/mswjs/interceptors/issues/123
 import 'setimmediate';
 
+import { setLogger } from 'react-query';
 import { server } from './mocks/server';
+
+// disable logging for react-query as it causes our test to fail
+setLogger({
+  log: () => {},
+  warn: () => {},
+  error: () => {},
+});
 
 // setup and teardown for our service mocks using msw
 beforeAll(() => server.listen());
